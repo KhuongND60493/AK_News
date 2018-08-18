f6527f8445fb3655d752b395c77429d5869ae2f3
KhuongND60493 <nguyenromio@gmail.com>
2018-08-18 10:03:24 +0700

    Update AK_RSS.js

--- AK_RSS.js
+++ AK_RSS.js
@@ -1,12 +1,17 @@
-list: [
+{
+categories: [
+        { id: 1, name: "Tin hot" },
+        { id: 2, name: "Thời sự" }
+    ],
+ rss: [
         {
             id: 1,
             website: "VNExpress",
             logo: "https://s.vnecdn.net/vnexpress/restruct/i/v61/graphics/img_logo_vne_web.gif",
             url: "https://vnexpress.net/",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "https://vnexpress.net/rss/tin-moi-nhat.rss" },
-                { id: 2, name: "Thời sự", rss: "https://vnexpress.net/rss/thoi-su.rss" }
+                { id: 1, url: "https://vnexpress.net/rss/tin-moi-nhat.rss" },
+                { id: 2, url: "https://vnexpress.net/rss/thoi-su.rss" }
             ]
         },
         {
@@ -15,8 +20,8 @@ list: [
             logo: "https://znews-static.zadn.vn/images/zing_footer_logo.jpg",
             url: "https://news.zing.vn/",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "https://news.zing.vn/rss/tin-moi.rss" },
-                { id: 2, name: "Thời sự", rss: "https://news.zing.vn/rss/thoi-su.rss" }
+                { id: 1, url: "https://news.zing.vn/rss/tin-moi.rss" },
+                { id: 2, url: "https://news.zing.vn/rss/thoi-su.rss" }
             ]
         },
         {
@@ -25,8 +30,8 @@ list: [
             url: "https://thanhnien.vn/",
             logo: "https://static.thanhnien.vn/v2/App_Themes/images/logo-tn-2.png",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "https://thanhnien.vn/rss/home.rss" },
-                { id: 2, name: "Thời sự", rss: "https://thanhnien.vn/rss/viet-nam.rss" }
+                { id: 1, url: "https://thanhnien.vn/rss/home.rss" },
+                { id: 2, url: "https://thanhnien.vn/rss/viet-nam.rss" }
             ]
         },
         {
@@ -35,8 +40,8 @@ list: [
             logo: "https://upload.wikimedia.org/wikipedia/vi/2/22/Vietnamnet-Logo.png",
             url: "http://vietnamnet.vn/",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "http://vietnamnet.vn/rss/home.rss" },
-                { id: 2, name: "Thời sự", rss: "http://vietnamnet.vn/rss/thoi-su.rss" }
+                { id: 1, url: "http://vietnamnet.vn/rss/home.rss" },
+                { id: 2, url: "http://vietnamnet.vn/rss/thoi-su.rss" }
             ]
         },
         {
@@ -45,8 +50,8 @@ list: [
             logo: "http://bvad.info/wp-content/uploads/2017/02/B%E1%BA%A3ng-gi%C3%A1-booking-qu%E1%BA%A3ng-c%C3%A1o-b%C3%A0i-Pr-banner-b%C3%A1o-nld.com_.png",
             url: "https://nld.com.vn/",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "https://nld.com.vn/tin-moi-nhat.rss" },
-                { id: 2, name: "Thời sự", rss: "https://nld.com.vn/thoi-su.rss" }
+                { id: 1, url: "https://nld.com.vn/tin-moi-nhat.rss" },
+                { id: 2, url: "https://nld.com.vn/thoi-su.rss" }
             ]
         },
         {
@@ -55,8 +60,7 @@ list: [
             logo: "http://image1.ictnews.vn/a/Assets/img/logo.png",
             url: "http://ictnews.vn",
             childs: [
-                { id: 2, name: "Thời sự", rss: "http://ictnews.vn/rss/thoi-su" },
-                { id: 1, name: "Hành chính" },
+                { id: 2, url: "http://ictnews.vn/rss/thoi-su" },
             ]
         },
         {
@@ -65,8 +69,8 @@ list: [
             logo: "http://quangcaoviet.info/wp-content/uploads/2017/05/dantri.jpg",
             url: "http://dantri.com.vn",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "http://dantri.com.vn/trangchu.rss" },
-                { id: 2, name: "Thời sự", rss: "http://dantri.com.vn/xa-hoi/chinh-tri.rss" }
+                { id: 1, url: "http://dantri.com.vn/trangchu.rss" },
+                { id: 2, url: "http://dantri.com.vn/xa-hoi/chinh-tri.rss" }
             ]
         },
         {
@@ -75,8 +79,8 @@ list: [
             logo: "http://marketingreview.vn/wp-content/uploads/2017/03/b%C3%A1o-gi%C3%A1-qu%E1%BA%A3ng-c%C3%A1o-b%C3%A1o-soha.vn_.png",
             url: "http://soha.vn/",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "http://soha.vn/giai-tri.rss" },
-                { id: 2, name: "Thời sự", rss: "http://soha.vn/thoi-su.rss" }
+                { id: 1, url: "http://soha.vn/giai-tri.rss" },
+                { id: 2, url: "http://soha.vn/thoi-su.rss" }
             ]
         },
         {
@@ -85,8 +89,8 @@ list: [
             logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5COIzyOHPhL1YJyOmNIj7NHpt2mE0f6Ttj7F_j1U-9dJJj34h",
             url: "https://www.24h.com.vn",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "https://www.24h.com.vn/upload/rss/trangchu24h.rss" },
-                { id: 2, name: "Thời sự", rss: "http://soha.vn/thoi-su.rss" }
+                { id: 1, url: "https://www.24h.com.vn/upload/rss/trangchu24h.rss" },
+                { id: 2, url: "http://soha.vn/thoi-su.rss" }
             ]
         },
         {
@@ -95,7 +99,7 @@ list: [
             logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSugfv-lODjIWVNZFpOOR2OCE6sqvDX_hWu5M_jIQ6tWwH9_mCs",
             url: "https://tinhte.vn",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "https://tinhte.vn/rss/" },
+                { id: 1, url: "https://tinhte.vn/rss/" },
 
             ]
         }, {
@@ -104,7 +108,7 @@ list: [
             logo: "http://www.techrum.vn/chevereto/images/2017/03/24/C6MRr.png",
             url: "http://www.techrum.vn",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "http://www.techrum.vn/forums/-/index.rss" },
+                { id: 1, url: "http://www.techrum.vn/forums/-/index.rss" },
 
             ]
         }, {
@@ -113,7 +117,7 @@ list: [
             logo: "http://bvad.info/wp-content/uploads/2017/02/B%E1%BA%A3ng-gi%C3%A1-booking-qu%E1%BA%A3ng-c%C3%A1o-b%C3%A0i-Pr-banner-b%C3%A1o-gamek.png",
             url: "http://gamek.vn",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "http://gamek.vn/home.rss" },
+                { id: 1, url: "http://gamek.vn/home.rss" },
             ]
         },
         {
@@ -122,8 +126,11 @@ list: [
             logo: "http://game4v.com/g4v-content/themes/game4v-2016/img/glogo16.png?12",
             url: "http://game4v.com",
             childs: [
-                { id: 1, name: "Trang chủ", rss: "http://game4v.com/feed" },
+                { id: 1, url: "http://game4v.com/feed" },
             ]
         },
 
-    ]
+    ]       
+}
+
+
